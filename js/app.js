11425861244cfe8d98d764d57f98dc248e175e27
Codexive_zech <zechariahhounwanou@gmail.com>
2022-05-16 09:11:57 +0100

    implemented the functionality that display fetched results fom the server into the HTML
    iterated over the list of wiki inputted value then destructured the needed properies for the display of the list they dynamically added it to the HTML

--- js/app.js
+++ js/app.js
@@ -26,5 +26,48 @@ form.addEventListener("submit", (e) => {
 
 // added the functionality that fetches data from the wiki server and display it in the app
 const fetchPage = async (searchValue) => {
-  console.log(searchValue);
+  //   dynamically display a loading msg when data is been fetched
+  result.innerHTML = `<div class="loading"></div>`;
+  // a try and catch block
+  try {
+    // fetches data from the server via the provided url and searched inputted value
+    const response = await fetch(`${url}${searchValue}`);
+    // converted the fetched data into a json format
+    const data = await response.json();
+    // destructured the fetched promise so as to get the arrays os inputted searched value
+    const result = data.query.search;
+    // checking to see if the length of the result array returned was less than 1
+    if (result.length < 1) {
+      //   dynamically displaying an error msg
+      result.innerHTML = `<div class="error">No Matching Result. Please Try Again</div>`;
+      return;
+    }
+    // invoking the func that displays the fetched data
+    renderResult(result);
+  } catch (error) {
+    //   dynamically display an error msg
+    result.innerHTML = `<div class="error">There was an Error</div>`;
+  }
+};
+
+const renderResult = (list) => {
+  // iterating over the list of data tha will display from the server
+  const wikiList = list
+    .map((item) => {
+      // destructuring the needed properties needed for thr app display
+      const { pageid, title, snippet } = item;
+      // dynamically returning a link and insert the destructured properties into it
+      return `
+          <a href="http://en.wikipedia.org/?curid=${pageid}" target="_blank">
+            <h4>${title}</h4>
+            <p>
+              ${snippet}
+            </p>
+          </a>
+        `;
+    })
+    .join("");
+
+  // dynamically inserting the iterated list of data from the sever into the HTML
+  result.innerHTML = `<div class="articles">${wikiList}</div>`;
 };
